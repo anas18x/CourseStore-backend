66398147d75ffb260f3f8ae45ea1a3efa3239dd3
anas18x <anasshakil00@gmail.com>
2025-11-20 23:34:05 +0530

    add: update user route endpoint

--- src/routes/router/user.route.js
+++ src/routes/router/user.route.js
@@ -1,18 +1,20 @@
-// import {Router} from "express";
-// const userRouter = Router()
-// import { AuthMiddleware } from "../../middlewares/index.js";
-// import { UserController } from "../../controller/index.js";
+import {Router} from "express";
+const userRouter = Router()
+import { AuthMiddleware } from "../../middlewares/index.js";
+import { UserController } from "../../controller/index.js";
 
 
-// userRouter.get("/my-courses",AuthMiddleware.verifyToken ,UserController.GetMyCourses)
+userRouter.get("/my-courses",AuthMiddleware.verifyToken ,UserController.GetMyCourses)
 
-// userRouter.get("/get-all-courses", AuthMiddleware.verifyToken ,CourseController.GetAllCourses)
+userRouter.get("/get-all-courses", AuthMiddleware.verifyToken ,CourseController.GetAllCourses)
 
-// userRouter.get("/get-course/:courseId", AuthMiddleware.verifyToken ,CourseController.GetCourseById)
+userRouter.get("/get-course/:courseId", AuthMiddleware.verifyToken ,CourseController.GetCourseById)
 
-// userRouter.get("/purchase-course/:courseId", AuthMiddleware.verifyToken ,UserController.PurchaseCourse)
+userRouter.get("/purchase-course/:courseId", AuthMiddleware.verifyToken ,UserController.PurchaseCourse)
 
-// userRouter.get("/cart", AuthMiddleware.verifyToken ,UserController.GetUserCart)
 
-// export default userRouter
+
+
+
+export default userRouter
 
